1fbcc92a395477d3f41c3c6743f5b550eff12697
Helio Andrade <hmajr.tec@gmail.com>
2025-09-12 08:46:15 +0000

    Add click event listener selection to a appointment

---
+++ src/modules/form/hours-click.js
@@ -0,0 +1,18 @@
+
+/**
+ * Track hour selection to a appointment
+ */
+export function hoursClick() {
+  const hours = document.querySelectorAll(".hour-available")
+
+  hours.forEach(( available) => {
+    available.addEventListener("click", (selected) => {
+      hours.forEach((hour) => {
+        hour.classList.remove("hour-selected")
+      })
+
+      
+      selected.target.classList.add("hour-selected")
+    })
+  })
+}
